fb0b18a7b5098a95f1ba8f724808a1dadf87bed1
donny88kurniawan88-cpu <donny88.kurniawan88@gmail.com>
2026-09-08 14:46:04 +0700

    Add files via upload

--- assets/bukti-generator/generate-bukti.js
+++ assets/bukti-generator/generate-bukti.js
@@ -2,8 +2,8 @@ const type=document.getElementById("gbType");
 const frame=document.getElementById("gbFrame");
 
 const pages={
-  "antar-bank":"/bukti-generator/generate-bukti-antar-bank.html?v=54.0.0",
-  "sesama-bca":"/bukti-generator/generate-bukti-sesama-bca.html?v=54.0.0"
+  "antar-bank":"/bukti-generator/generate-bukti-antar-bank.html?v=56.0.0",
+  "sesama-bca":"/bukti-generator/generate-bukti-sesama-bca.html?v=56.0.0"
 };
 
 type.addEventListener("change",()=>{
